9150616e87a5af560c2f8e1f746e2f6d8c9a3d00
Torey <Torey@TJH-DI5485>
2023-01-13 11:01:47 -0500

    Inital post

---
+++ SaturdaysGitPractice/src/everyoneIsHere/ReadMe.java
@@ -0,0 +1,10 @@
+package everyoneIsHere;
+
+public class ReadMe {
+	
+	//Pull the most recent project
+	//then create a class with your name
+	//and a some code. Push it back to 
+	//GitHub with your update.
+
+}
---
+++ SaturdaysGitPractice/src/everyoneIsHere/Torey.java
@@ -0,0 +1,8 @@
+package everyoneIsHere;
+
+public class Torey {
+	public static void main(String[] args) {
+		System.out.println("Hello, Team B!");
+	}
+
+}
